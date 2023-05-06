1104d69d260475f41638e6d9ff0e61e23e9e25c8
maliakash1617 <maliakash225@gmail.com>
2023-05-06 07:31:01 +0530

    hello
    hello

--- hello.js
+++ hello.js
@@ -1,6 +1,3 @@
-
-
-
 var myFullName;
 console.log(myFullName);
 myFullName = "Akash Mali";
@@ -9,10 +6,17 @@ myFullName = "Akash Sidram Mali";
 console.log(myFullName);
 var myLove = "I love only JS";
 console.log(myLove);
-var firstName = "Akash"; console.log(firstName);
-var lastName ="Mali";console.log(lastName);
-var age = "23";console.log(age);
-var address ="Madgyal"; console.log(address);
-var pincode = "416413";console.log(pincode);
-var gender ="Male"; console.log(gender);
-var isMarried = "No"; console.log(isMarried);
+var firstName = "Akash";
+console.log(firstName);
+var lastName = "Mali";
+console.log(lastName);
+var age = "23";
+console.log(age);
+var address = "Madgyal";
+console.log(address);
+var pincode = "416413";
+console.log(pincode);
+var gender = "Male";
+console.log(gender);
+var isMarried = "No";
+console.log(isMarried);

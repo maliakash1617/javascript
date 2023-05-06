380343f90e1f98493e1bfca017f091cd99b088ed
maliakash1617 <maliakash225@gmail.com>
2023-05-06 07:28:52 +0530

    new
    new

---
+++ hello.js
@@ -0,0 +1,18 @@
+
+
+
+var myFullName;
+console.log(myFullName);
+myFullName = "Akash Mali";
+console.log(myFullName);
+myFullName = "Akash Sidram Mali";
+console.log(myFullName);
+var myLove = "I love only JS";
+console.log(myLove);
+var firstName = "Akash"; console.log(firstName);
+var lastName ="Mali";console.log(lastName);
+var age = "23";console.log(age);
+var address ="Madgyal"; console.log(address);
+var pincode = "416413";console.log(pincode);
+var gender ="Male"; console.log(gender);
+var isMarried = "No"; console.log(isMarried);
